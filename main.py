e6aeaac629e1559902ba810969b2d88b7b71a38b
Nicken Shidqia Nurahman <138204220+nickenshidqia@users.noreply.github.com>
2023-12-27 11:24:56 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,42 @@
+from flask import Flask, render_template
+import pandas as pd
+
+app = Flask(__name__)
+
+stations = pd.read_csv("data_small/stations.txt", skiprows=17)
+
+#homepage
+@app.route("/")
+def home():
+    return render_template("home.html", data=stations.to_html())
+
+#show temperature based on station & date
+@app.route("/api/v1/<station>/<date>")
+def about(station, date):
+    filename = "data_small/TG_STAID" + str(station).zfill(6) + ".txt"
+    df = pd.read_csv(filename, skiprows=20, parse_dates=["    DATE"])
+    temperature = df.loc[df["    DATE"] == date]["   TG"].squeeze() / 10
+    return {"station": station,
+            "date": date,
+            "temperature": temperature}
+
+#show data for 1 particular station
+@app.route("/api/v1/<station>")
+def all_data(station):
+    filename = "data_small/TG_STAID" + str(station).zfill(6) + ".txt"
+    df = pd.read_csv(filename, skiprows=20, parse_dates=["    DATE"])
+    result = df.to_dict(orient="records")
+    return result
+
+#show data for 1 particular year
+@app.route("/api/v1/yearly/<station>/<year>")
+def yearly(station, year):
+    filename = "data_small/TG_STAID" + str(station).zfill(6) + ".txt"
+    df = pd.read_csv(filename, skiprows=20)
+    df["    DATE"] = df["    DATE"].astype(str)
+    result = df[df["    DATE"].str.startswith(str(year))].to_dict(orient="records")
+    return result
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
